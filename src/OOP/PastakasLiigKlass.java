452f93606021b630377d72b916e4258e550675b2
refocus <refocustest@gmail.com>
2017-01-23 15:03:58 +0200

    Add files via upload

---
+++ src/OOP/PastakasLiigKlass.java
@@ -0,0 +1,23 @@
+package OOP;
+
+/**
+ * Created by krist on 23.01.17.
+ */
+public class PastakasLiigKlass {
+
+
+    public static void main(String[] args) {     // STANDARD see on safe MAIN meetod mis käivitab igat klassi automaatselt
+
+        //kirjuta     //miks ta ei leia seda meetodit kirjuta? seda imestas see kes mind aitas IIk Cybersecurity,
+        // et oleks pidanud leidma üles sõne kirjuta, mis on teises klassis meetod
+        // ta ytles, et oleks pidanud saama kätte küll Pastakas klassi meetodeid
+        //ytles, iga KONSTRUKTOR meetod ehk igaüks neist on nagu eraldiseisev üksus, eraldi muinasjutud muinasjutu kogumiku raamatust
+        //ning et neid peaks mujalt kätte ligi saada välja kutsudes
+
+
+        //tint;
+
+
+    }
+
+}
